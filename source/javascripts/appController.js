f0c85f04199ada4679d366eff5494c5ece85e1ab
cdeliens <cdeliens@gmail.com>
2013-05-22 23:58:08 -0600

    transition effect

--- source/javascripts/appController.js
+++ source/javascripts/appController.js
@@ -1,9 +1,9 @@
 function appController($scope) {
   $scope.options = [
-    {background: "bg1", link: "#", text: "Link 1"},
-    {background: "bg2", link: "#", text: "Link 2"},
-    {background: "bg1", link: "#", text: "Link 3"},
-    {background: "bg2", link: "#", text: "Link 4"}];
+    {background: "bg1", link: "#", text: "Link 1", effect: "fade"},
+    {background: "bg2", link: "#", text: "Link 2", effect: "fade"},
+    {background: "bg1", link: "#", text: "Link 3", effect: "fade"},
+    {background: "bg2", link: "#", text: "Link 4", effect: "fade"}];
   
   $scope.active_background = $scope.options[0].background;
 
